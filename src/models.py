a086c1f997bfcf74d2b55e6bfe78d2e137f16f59
JuanAntonaccio <jmantonaccio@gmail.com>
2021-10-10 23:10:16 +0000

    datos cargados

--- src/models.py
+++ src/models.py
@@ -1,11 +1,11 @@
 import os
 import sys
-import enum
 from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
+import enum
 
 Base = declarative_base()
 
